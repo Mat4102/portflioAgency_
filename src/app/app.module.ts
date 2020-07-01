0794e4134fdde9e4aad2856a505b8e8d218dc008
Ferchichi Hassen <ferchichi.has@gmail.com>
2020-07-01 18:03:08 +0200

    first commit home page 0107

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomeLadureeComponent } from './home-laduree/home-laduree.component';
+import { HomeEssilorComponent } from './home-essilor/home-essilor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeLadureeComponent,
+    HomeEssilorComponent
   ],
   imports: [
     BrowserModule,
