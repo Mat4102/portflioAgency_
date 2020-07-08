65e1296f0cbb1340b33d88c6b0dd1b6423a0a349
Ferchichi Hassen <ferchichi.has@gmail.com>
2020-07-08 17:37:25 +0200

    Add Home Page and first animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HomeLadureeComponent } from './home-laduree/home-laduree.component';
 import { HomeEssilorComponent } from './home-essilor/home-essilor.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HomeEssilorComponent } from './home-essilor/home-essilor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {trigger, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('3s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('3s', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HomeComponent {
 
 }
+
+// Exemple animation with query selecteur
+/*
+*   animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        query('h1', style({ opacity: 0 })),
+        // animate the inner elements in, one by one
+        query('h1', animate('15s', style({ opacity: 1 }))),
+      ]),
+      transition(':leave', [
+        animate('10s', style({ opacity: 1 }))
+      ])
+  ])
+  ]*/
