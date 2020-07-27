2a065d61754170e4f72177823a7d685f74b946ad
Houssemddinmezghich <mezghich.mez@gmail.com>
2020-07-27 11:23:00 +0200

    home_agency

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {HomeLadureeComponent} from './home-laduree/home-laduree.component';
+import { HomeAgencyComponent } from './agency/home-agency/home-agency.component';
 
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent },
-  { path: 'homeLaduree', component: HomeLadureeComponent }];
+  { path: 'homeLaduree', component: HomeLadureeComponent },
+  { path: 'home/homeAgency', component: HomeAgencyComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
